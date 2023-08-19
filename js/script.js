d15ddb9cc8053e4e9e99d4d4e49f29959cd20de3
carozum <czumbiehlfaure@hotmail.fr>
2023-08-19 12:21:02 +0200

    add exceed requirements to the project

--- js/script.js
+++ js/script.js
@@ -9,44 +9,51 @@ const quotes = [
   {
     quote: "Act as if what you do makes a difference. It does.",
     source: "William James",
-    citation:"The principles of psychology",
-    year:"1890"
+    citation: "The principles of psychology",
+    year: "1890",
+    tags: ["success", "life"]
   },
   {
-    quote:"Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    quote: "Success is not final; failure is not fatal: It is the courage to continue that counts.",
     source: "Winston S. Churchill",
-    citation:"",
-    year:""
+    citation: "",
+    year: "",
+    tags: ["success"]
   },
   {
-    quote:"I never dreamed about success. I worked for it",
-    source:"Estée Lauder",
+    quote: "I never dreamed about success. I worked for it",
+    source: "Estée Lauder",
     citation:"",
-    year:""
+    year: "",
+    tags: ["success", "dreams"]
   },
   {
-    quote:"The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
-    source:"Winston S. Churchill",
-    citation:"",
-    year:""
+    quote: "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    source: "Winston S. Churchill",
+    citation: "",
+    year: "",
+    tags: ["success", "difficulty"]
   },
   {
-    quote:"If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.",
-    source:"Steve Jobs",
-    citation:"",
-    year:""
+    quote: "If you are working on something that you really care about, you don’t have to be pushed. The vision pulls you.",
+    source: "Steve Jobs",
+    citation: "",
+    year: "",
+    tags: ["work", "vision"]
   },
   {
-    quote:"Experience is a hard teacher because she gives the test first, the lesson afterwards.",
-    source:"Vernon Sanders Law",
-    citation:"",
-    year:""
+    quote: "Experience is a hard teacher because she gives the test first, the lesson afterwards.",
+    source: "Vernon Sanders Law",
+    citation: "",
+    year: "",
+    tags: ["work", "life"]
   },
   {
-    quote:"Setting goals is the first step in turning the invisible into the visible.",
-    source:"Tony Robbins",
-    citation:"Unlimited Power",
-    year:"1986"
+    quote: "Setting goals is the first step in turning the invisible into the visible.",
+    source: "Tony Robbins",
+    citation: "Unlimited Power",
+    year: "1986",
+    tags: ["life", "goal"]
   }
 ];
 
@@ -62,6 +69,24 @@ function getRandomQuote(quotes){
   return quotes[randomIndex];
 }
 
+/**
+ * generate random number between 0 and 255
+ */
+function randomNumber(num){
+  return Math.floor(Math.random() * (num+1));
+}
+
+/**
+ * change the color of the background and of the font
+ * 
+ */
+function changeColors(){
+  const red = randomNumber(255);
+  const green = randomNumber(255);
+  const blue = randomNumber(255);
+  document.querySelector("body").style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  //document.querySelector("body").style.color = `rgb(${255-red}, ${255-green}, ${255-blue})`;
+}
 
 
 /**
@@ -88,12 +113,23 @@ function printQuote(){
     message += `<span class="year">${currentQuote.year} </span> ` ;
   }
 
+  //check if the quote has tags
+  if (currentQuote.tags){
+    for (let i=0; i < currentQuote.tags.length; i++){
+      message += `<span class="tags"> #${currentQuote.tags[i]} </span> `
+    }
+  }
+
+  //change the changeColors
+  changeColors();
+
   //display the message
   message += "</p>";
   document.getElementById('quote-box').innerHTML = message;
 }
 
-
+// print a new quote at regular interval
+setInterval(printQuote, 5000);
 
 //click event listener for the print quote button
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
